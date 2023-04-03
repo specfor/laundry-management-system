8522d77f83b0ca1bcaad8ba8955ba8cc21feca0d
Chanuka Nuhan <chanukanuhan@gmail.com>
2023-04-03 23:13:16 +0530

    dashboard

--- desktop-app/assets/js/dashboard.js
+++ desktop-app/assets/js/dashboard.js
@@ -1,8 +1,21 @@
 
 window.addEventListener("load",function(){
     
-    document.getElementById("dataSubmit").addEventListener("click",function(){
-        console.log(document.getElementById("companyDrop").value)
-    })
+    //document.getElementById("dataSubmit").addEventListener("click",autoFill)
+    document.getElementById("continue").addEventListener("click",sendDataToTheServer)
 
 })
+
+
+function sendDataToTheServer(){
+   let firstName = document.getElementById("fname").value
+   let lastName = document.getElementById("lname").value
+   let email = document.getElementById("email").value
+   let mobileNumber = document.getElementById("mobiNum").value
+   let address = document.getElementById("address").value
+   let urgency = document.getElementById("urgency").value
+   let from = document.getElementById("from").value
+   let to = document.getElementById("to").value
+
+   console.log(`${firstName} ${lastName} ${email} ${mobileNumber} ${address} ${urgency} ${from} ${to}`)
+}
