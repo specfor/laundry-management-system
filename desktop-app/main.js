af4d1d3a4d06247d4bdcd5a17f83931af1d3160e
pycodecrusher <75834116+pycodecrusher@users.noreply.github.com>
2023-04-13 23:43:34 +0530

    +fixes

--- desktop-app/main.js
+++ desktop-app/main.js
@@ -26,7 +26,7 @@ const createMainWindow = () => {
     // and load the index.html of the app.
     mainWindow.loadFile(__dirname + '/html/dashboard.html')
     //Open the DevTools.
-    //mainWindow.webContents.openDevTools()
+    mainWindow.webContents.openDevTools()
 }
 
 ipcMain.on("clientData",function(event,data){
