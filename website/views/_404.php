60a96df50d24e054dc56b1ffd3cf1bd11996d61c
vihanga <vihanga2003nimsara@gmail.com>
2023-04-28 11:51:21 +0530

    "404 page not found" page added

---
+++ website/views/_404.php
@@ -0,0 +1,26 @@
+<?php use LogicLeap\StockManagement\core\TailwindUiRenderer; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/css/styles.css">
+    <title>Page Not Found</title>
+</head>
+<body>
+<div class="xs:container">
+    <div>
+        <div class="grid grid-cols-1 xs:grid-cols-2 h-screen items-center">
+            <div>
+                <img src="/assets/images/404.jpg" alt="Page Not Found">
+            </div>
+            <div class="xs:justify-center order-first xs:order-none">
+                <h1 class="font-bold text-6xl text-center">Page Not Found</h1>
+                <h4 class="text-center text-2xl mt-5">Try correcting the url.</h4>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
