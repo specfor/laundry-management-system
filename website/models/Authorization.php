b44abdcebfb24d2ea5d6cb6d16d27d8f8cd93918
vihanga <vihanga2003nimsara@gmail.com>
2023-04-30 23:22:09 +0530

    bug fix

--- website/models/Authorization.php
+++ website/models/Authorization.php
@@ -2,14 +2,15 @@
 
 namespace LogicLeap\StockManagement\models;
 
+use LogicLeap\StockManagement\core\Application;
 use PDO;
 
-class Authorization extends DbModel
+class Authorization
 {
     public static function getUserRole(string $authToken): int
     {
         $sql = "SELECT role FROM users where id=(select user_id from user_status where auth_token='$authToken')";
-        $statement = self::prepare($sql);
+        $statement = Application::$app->db->pdo->prepare($sql);
         $statement->execute();
         return $statement->fetch(PDO::FETCH_ASSOC)['role'];
     }
