f8a9bb0b2c58fe94d132d3388838ec3d2a1340e3
vihanga <vihanga2003nimsara@gmail.com>
2023-05-22 19:15:39 +0530

    vue configured for tailwind

---
+++ vue-web-design/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <h3 class="text-4xl text-orange-700">Hello world.</h3>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue-web-design/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{vue,js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            container: {
+                center: true,
+                padding: '2rem'
+            },
+            extend: {
+                screens: {
+                    'xs': '475px'
+                }
+            },
+        },
+    },
+    plugins: [],
+}
+
