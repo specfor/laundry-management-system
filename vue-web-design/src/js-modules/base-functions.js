0f69af6f721d71c5fc4c8ce28ca593fa4a81df14
vihanga <vihanga2003nimsara@gmail.com>
2023-05-29 12:45:45 +0530

    updated the get and post request functions

--- vue-web-design/src/js-modules/base-functions.js
+++ vue-web-design/src/js-modules/base-functions.js
@@ -1,18 +1,56 @@
-export async function sendJsonPostRequest(url, jsonBody, headers = []) {
+import {apiBaseUrl} from "./website-constants.js";
+
+export async function sendJsonPostRequest(url, jsonBody, headers = window.httpHeaders, jsonifyResponse = true) {
     headers['Content-Type'] = 'application/json'
-    return await fetch(url, {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify(jsonBody),
-        credentials: "same-origin"
-    })
+
+    let response = null
+    try {
+        response =  await fetch(url, {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(jsonBody),
+            credentials: "same-origin"
+        })
+    }catch (e){
+        return {status: 'error', message: 'Failed to connect to the servers.', data: null}
+    }
+
+    if (response.status !== 200)
+        return {status: 'error', message: 'Something went wrong.', data: null}
+
+    let data_ = null
+    if (jsonifyResponse){
+        data_ = await response.json()
+        return {status: data_.statusMessage, data: data_.body, message: data_.body.message ?? null}
+    }
+    else{
+        data_ = await response.text()
+        return {status: 'success', data: data_}
+    }
 }
 
-export async function sendGetRequest(url, params, headers) {
+export async function sendGetRequest(url, params, headers = window.httpHeaders, jsonifyResponse = true) {
+    let response = null
+    try {
+        response = await fetch(url, {
+            method: 'GET',
+            headers: headers,
+            credentials: "same-origin"
+        })
+    }catch (e){
+        return {status: 'error', message: 'Failed to connect to the servers.', data: null}
+    }
 
-    return await fetch(url, {
-        method: 'GET',
-        headers: headers,
-        credentials: "same-origin"
-    })
+    if (response.status !== 200)
+        return {status: 'error', message: 'Something went wrong.', data: null}
+
+    let data_ = null
+    if (jsonifyResponse){
+        data_ = await response.json()
+        return {status: data_.statusMessage, data: data_.body, message: data_.body.message ?? null}
+    }
+    else{
+        data_ = await response.text()
+        return {status: 'success', data: data_}
+    }
 }
