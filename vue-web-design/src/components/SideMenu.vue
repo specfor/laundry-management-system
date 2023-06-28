3c71fcc42de910b392e079746d2cfd75f3374da2
vihanga <vihanga2003nimsara@gmail.com>
2023-06-28 13:09:33 +0530

    menu bar ui mini fix

--- vue-web-design/src/components/SideMenu.vue
+++ vue-web-design/src/components/SideMenu.vue
@@ -1,41 +1,41 @@
 <template>
   <div class="h-screen bg-gray-500 w-[200px] text-lg font-semibold fixed pl-1">
-    <RouterLink to="/dashboard" class="flex w-full px-2 py-2 rounded-l-lg hover:bg-slate-700 items-center"
+    <RouterLink to="/dashboard" class="flex w-full px-2 py-1 rounded-l-lg hover:bg-slate-700 items-center"
                 active-class="active-side-menu-element">
       <HomeIcon class="w-6 h-6 mr-3"/>
       Dashboard
     </RouterLink>
-    <RouterLink to="/orders" class="flex w-full px-2 py-2 rounded-l-lg hover:bg-slate-700 items-center"
+    <RouterLink to="/orders" class="flex w-full px-2 py-1 rounded-l-lg hover:bg-slate-700 items-center"
                 active-class="active-side-menu-element">
       <RectangleStackIcon class="w-6 h-6 mr-3"/>
       Orders
     </RouterLink>
-    <RouterLink to="/payments" class="flex w-full px-2 py-2 rounded-l-lg hover:bg-slate-700 items-center"
+    <RouterLink to="/payments" class="flex w-full px-2 py-1 rounded-l-lg hover:bg-slate-700 items-center"
                 active-class="active-side-menu-element">
       <CreditCardIcon class="w-6 h-6 mr-3"/>
       Payments
     </RouterLink>
-    <RouterLink to="/customers" class="flex w-full px-2 py-2 rounded-l-lg hover:bg-slate-700 items-center"
+    <RouterLink to="/customers" class="flex w-full px-2 py-1 rounded-l-lg hover:bg-slate-700 items-center"
                 active-class="active-side-menu-element">
       <UserGroupIcon class="w-6 h-6 mr-3"/>
       Customers
     </RouterLink>
-    <RouterLink to="/products" class="flex w-full px-2 py-2 rounded-l-lg hover:bg-slate-700 items-center"
+    <RouterLink to="/products" class="flex w-full px-2 py-1 rounded-l-lg hover:bg-slate-700 items-center"
                 active-class="active-side-menu-element">
       <ArchiveBoxIcon class="w-6 h-6 mr-3"/>
       Products
     </RouterLink>
-    <RouterLink to="/branches" class="flex w-full px-2 py-2 rounded-l-lg hover:bg-slate-700 items-center"
+    <RouterLink to="/branches" class="flex w-full px-2 py-1 rounded-l-lg hover:bg-slate-700 items-center"
                 active-class="active-side-menu-element">
       <BuildingOffice2Icon class="w-6 h-6 mr-3"/>
       Branches
     </RouterLink>
-    <RouterLink to="/employees" class="flex w-full px-2 py-2 rounded-l-lg hover:bg-slate-700 items-center"
+    <RouterLink to="/employees" class="flex w-full px-2 py-1 rounded-l-lg hover:bg-slate-700 items-center"
                 active-class="active-side-menu-element">
       <IdentificationIcon class="w-6 h-6 mr-3"/>
       Employees
     </RouterLink>
-    <RouterLink to="/users" class="flex w-full px-2 py-2 rounded-l-lg hover:bg-slate-700 items-center"
+    <RouterLink to="/users" class="flex w-full px-2 py-1 rounded-l-lg hover:bg-slate-700 items-center"
                 active-class="active-side-menu-element">
       <UsersIcon class="w-6 h-6 mr-3"/>
       Users
