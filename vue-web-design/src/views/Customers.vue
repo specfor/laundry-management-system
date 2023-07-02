cfeb132df6325f384f5951b94ef7c34a50cd4b08
vihanga <vihanga2003nimsara@gmail.com>
2023-07-02 15:52:15 +0530

    customer input field validations added

--- vue-web-design/src/views/Customers.vue
+++ vue-web-design/src/views/Customers.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="flex justify-between mt-5 mb-3">
     <h3 class="text-2xl font-semibold">Branches</h3>
-    <button class="bg-slate-600 text-slate-100 rounded-md py-2 px-3 font-semibold" @click="addNewCustomer">+ New Customer</button>
+    <button class="bg-slate-600 text-slate-100 rounded-md py-2 px-3 font-semibold" @click="addNewCustomer">+ New
+      Customer
+    </button>
   </div>
 
   <TableComponent :tableColumns="customersTableCol" :tableRows="customersTableRows" :actions="customersTableActions"
@@ -15,13 +17,14 @@ import {ref} from 'vue'
 import {sendGetRequest, sendJsonPostRequest} from "../js-modules/base-functions.js";
 import {apiBaseUrl} from "../js-modules/website-constants.js";
 import {PencilSquareIcon, TrashIcon} from "@heroicons/vue/24/solid/index.js";
+import {validateInput} from "../js-modules/form-validations.js";
 
 let customersTableCol = ['Id', 'Customer Name', 'Phone Number', 'Email', 'Address', 'Joined Date',
   'Banned', 'Modifications']
 let customersTableRows = ref([])
 let customersTableActions = [
-    {onClickEvent: 'editCustomer', btnText: 'Edit', type: 'icon', icon: PencilSquareIcon, iconColor: 'fill-blue-700'},
-    {onClickEvent: 'removeCustomer', btnText: 'Remove', type: 'icon', icon: TrashIcon, iconColor: 'fill-red-700'}
+  {onClickEvent: 'editCustomer', btnText: 'Edit', type: 'icon', icon: PencilSquareIcon, iconColor: 'fill-blue-700'},
+  {onClickEvent: 'removeCustomer', btnText: 'Remove', type: 'icon', icon: TrashIcon, iconColor: 'fill-red-700'}
 ]
 
 async function getCustomers() {
@@ -32,7 +35,7 @@ async function getCustomers() {
     let customers = response.data["customers"];
     for (const customer of customers) {
       customersTableRows.value.push([customer['customer_id'], customer['name'], customer['phone_num'],
-        customer['email'], customer['address'], customer['joined_date'], customer['banned']])
+        customer['email'], customer['address'], customer['joined_date'], customer['banned'] ? 'Yes' : 'No'])
     }
   } else {
     window.errorNotification('Fetch Customer Data', response.message)
@@ -44,9 +47,9 @@ getCustomers()
 async function addNewCustomer() {
   let customer = await window.addNewForm('New Customer', 'Add', [
     {name: 'name', text: 'Customer Name', type: 'text'},
-    {name: 'email', text: 'Email', type: 'email'},
+    {name: 'email', text: 'Email', type: 'email', validate: value => validateInput(value, 'email')},
     {name: 'address', text: 'Address', type: 'textarea'},
-    {name: 'phone', text: 'Phone Number', type: 'text'}
+    {name: 'phone', text: 'Phone Number', type: 'text', validate: value => validateInput(value, 'phone-number')}
   ])
 
   if (!customer['accepted'])
@@ -59,17 +62,11 @@ async function addNewCustomer() {
     "address": customer['data']['address']
   }, window.httpHeaders)
 
-  if (response.status === 200) {
-    let data = await response.json()
-
-    if (data.statusMessage === "success") {
-      getCustomers()
-      window.successNotification('Add New Customer', data.body.message)
-    } else {
-      window.errorNotification('Add New Customer', data.body.message)
-    }
+  if (response.status === "success") {
+    getCustomers()
+    window.successNotification('Add New Customer', response.message)
   } else {
-    window.errorNotification('Add New Customer', 'Something went wrong. Can not fetch data.')
+    window.errorNotification('Add New Customer', response.message)
   }
 }
 
@@ -77,17 +74,20 @@ async function editCustomer(id) {
   let customerData = customersTableRows.value.filter((row) => {
     return row[0] === id
   })[0]
-  let banned = 'no'
-  if (customerData[6] === true)
-    banned = 'yes'
 
   let customer = await window.addNewForm('Update Customer', 'Update', [
     {name: 'name', text: 'Customer Name', type: 'text', value: customerData[1]},
-    {name: 'phone', text: 'Phone Number', type: 'text', value: customerData[2]},
-    {name: 'email', text: 'Email', type: 'email', value: customerData[3]},
+    {
+      name: 'email', text: 'Email', type: 'email', value: customerData[3],
+      validate: value => validateInput(value, 'email')
+    },
+    {
+      name: 'phone', text: 'Phone Number', type: 'text', value: customerData[2],
+      validate: value => validateInput(value, 'phone-number')
+    },
     {name: 'address', text: 'Address', type: 'textarea', value: customerData[4]},
     {
-      name: 'banned', text: 'Ban Customer', type: 'select', value: banned,
+      name: 'banned', text: 'Ban Customer', type: 'select', value: customerData[6].toLowerCase(),
       options: [{value: 'yes', text: 'Yes'}, {value: 'no', text: 'No'}]
     }
   ])
@@ -104,47 +104,36 @@ async function editCustomer(id) {
     'banned': ((customer['data']['banned'] === 'yes'))
   }, window.httpHeaders)
 
-  if (response.status === 200) {
-    let data = await response.json()
-
-    if (data.statusMessage === "success") {
-      customersTableRows.value.filter((row) => {
-        if (row[0] === id) {
-          row[1] = customer['data']['name']
-          row[2] = customer['data']['phone']
-          row[3] = customer['data']['email']
-          row[4] = customer['data']['address']
-          row[6] = customer['data']['banned']
-          return row
-        }
-      })
-      window.successNotification('Update Branch', data.body.message)
-    } else {
-      window.errorNotification('Update Branch', data.body.message)
-    }
+  if (response.status === "success") {
+    customersTableRows.value.filter((row) => {
+      if (row[0] === id) {
+        row[1] = customer['data']['name']
+        row[2] = customer['data']['phone']
+        row[3] = customer['data']['email']
+        row[4] = customer['data']['address']
+        row[6] = customer['data']['banned']
+        return row
+      }
+    })
+    window.successNotification('Update Branch', response.message)
   } else {
-    window.errorNotification('Update Branch', 'Something went wrong. Can not fetch data.')
+    window.errorNotification('Update Branch', response.message)
   }
 }
 
 async function deleteCustomer(id) {
   let confirm = await window.popupConfirmation('Delete Customer',
-      'This action is irreversible. Are you sure you want to remove this customer?')
+    'This action is irreversible. Are you sure you want to remove this customer?')
   if (confirm === true) {
     let response = await sendJsonPostRequest(apiBaseUrl + "/customers/delete", {
       'customer-id': id
     }, window.httpHeaders)
 
-    if (response.status === 200) {
-      let data = await response.json()
-      if (data.statusMessage === "success") {
-        getCustomers()
-        window.successNotification('Delete Customer', data.body.message)
-      } else {
-        window.errorNotification('Delete Customer', data.body.message)
-      }
+    if (response.status === "success") {
+      getCustomers()
+      window.successNotification('Delete Customer', response.message)
     } else {
-      window.errorNotification('Delete Customer', 'Something went wrong. Can not fetch data.')
+      window.errorNotification('Delete Customer', response.message)
     }
   }
 }
