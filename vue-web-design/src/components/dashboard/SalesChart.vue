dedcc6c30d88d6e5cddc3a896fe003f21dfd542d
vihanga <vihanga2003nimsara@gmail.com>
2023-07-07 15:51:32 +0530

    daily sales count chart funtionality added

--- vue-web-design/src/components/dashboard/SalesChart.vue
+++ vue-web-design/src/components/dashboard/SalesChart.vue
@@ -1,54 +1,58 @@
 <script setup>
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-} from 'chart.js'
-import { Line } from 'vue-chartjs'
+import {Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend} from 'chart.js'
+import {Line} from 'vue-chartjs'
+import {sendGetRequest} from '../../js-modules/base-functions.js'
+import {apiBaseUrl} from '../../js-modules/website-constants.js'
+import {ref} from "vue";
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-)
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
 
-const data = {
-  labels: [
-    '',
-    '',
-    'Wednesday',
-    '',
-    '',
-    'Saturday',
-    'Sunday'
-  ],
+let label = []
+let orderCount = []
+let showGraph = ref(false)
+
+async function getOrderCounts() {
+  let response = await sendGetRequest(apiBaseUrl + '/orderCount?branch-id=0&no-days-backward=10')
+  if (response.status === 'success') {
+    for (const key of Object.keys(response.data['order-counts'])) {
+      label.push(key)
+      orderCount.push(response.data['order-counts'][key])
+    }
+    showGraph.value = true;
+    console.log(orderCount)
+  }
+}
+
+getOrderCounts()
+
+let data = {
+  labels: label,
   datasets: [
     {
-      label: 'Data One',
+      label: 'Orders',
       backgroundColor: '#0f71d5',
-      data: [40, 20, 12, 39, 10, 40, 39]
+      data: orderCount
     }
   ]
 }
 
-const options = {
+let options = {
   responsive: true,
-  maintainAspectRatio: false
+  maintainAspectRatio: false,
+  scales: {
+    y: {
+      min: 0
+    },
+    x: {
+      reverse: true
+    }
+  }
 }
 
 </script>
 
 <template>
-  <Line :data="data" :options="options"/>
+  <Line :data="data" :options="options" v-if="showGraph"/>
 </template>
 
 <style scoped>
