46c6b2c714e9cd42abb7080501fdd2f30b07939c
vihanga <vihanga2003nimsara@gmail.com>
2023-08-10 15:46:09 +0530

    added package init api endpoint

---
+++ api/models/accounting/InitAccountingPackage.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace LogicLeap\StockManagement\models\accounting;
+
+use LogicLeap\StockManagement\models\DbModel;
+
+class InitAccountingPackage extends DbModel
+{
+    public static function init(): bool
+    {
+        $taxExempt = Taxes::getTaxes(taxName: 'tax exempt');
+        if (empty($taxExempt))
+            $taxId = Taxes::createTax('tax exempt', 0)['tax_id'];
+        else
+            $taxId = $taxExempt[0]['tax_id'];
+
+        $taxAccount = Accounting::getAccounts(name:'sales tax');
+        if (empty($taxAccount))
+            Accounting::createAccount('sales tax', 't000', 'Current Liability', $taxId,
+                'This is the account used to track sales taxes on income and expenses. This software is designed 
+            to use only one sales tax account.', false);
+
+        return true;
+    }
+}
