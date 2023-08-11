8cf2d19a3a04f273b47488e8c427085da08619ca
gehirudm <gehirudm@pm.me>
2023-08-12 01:33:58 +0530

    API Response Interfaces

--- accounting-web/src/types/index.d.ts
+++ accounting-web/src/types/index.d.ts
@@ -1,18 +1,42 @@
+// API Response Interfaces
+
 export interface LedgerRecord {
-    record_id: number,
-    account_id: number,
-    reference: string,
-    description: string,
-    credit: number,
-    debit: number,
-    tax: number,
+    record_id: number
+    account_id: number
+    reference: string
+    description: string
+    credit: number
+    debit: number
+    tax: number
     timestamp: Date
 }
+
+export interface Tax {
+    tax_id: number
+    name: string
+    description: string
+    tax_rate: string
+}
+
+export interface FinancialAccount {
+    account_id: number
+    name: string
+    type: string
+    code: string
+    description: string
+    tax_id: number
+    archived: number
+    deletable: number
+}
+
+// API Request Interfaces
+
+
 
 import { DefineComponent } from "vue";
 import { Object } from 'ts-toolbelt'
 
-export type ComponentOptions<T> = T extends DefineComponent<A, infer P, B> ? P extends { $props: infer Props } ? Object.Writable<Props> : never : never;
+export type ComponentOptions<T> = T extends DefineComponent<infer _A, infer P, infer _B> ? P extends { $props: infer Props extends object } ? Object.Writable<Props> : never : never;
 
 // export type ComponentOptions<T> = T extends DefineComponent<A, infer O, B> ?
 //     O extends { $emit: infer E, $props: infer P } ?
