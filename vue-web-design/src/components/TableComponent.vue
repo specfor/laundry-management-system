a32d8018699ef827bfd2ccf6168f99ccd246208a
vihanga <vihanga2003nimsara@gmail.com>
2023-08-17 12:45:39 +0530

    reformatted the code

--- vue-web-design/src/components/TableComponent.vue
+++ vue-web-design/src/components/TableComponent.vue
@@ -1,31 +1,40 @@
 <template>
   <div class="w-full overflow-x-auto">
-     <div class="flex justify-between w-full">
-        <div class="flex">
-          <button :disabled="isDisabled" class="rounded-md px-3 py-1 transition duration-300  mb-4 subpixel-antialiased	font-medium	" :class="{'bg-red-600 text-white hover:bg-red-700':isActive,'bg-stone-200 text-stone-600':!isActive}" @click="()=>{$emit(deleteMultiple[0]['onClickEvent'],selectedIds);isDisabled = true;isActive=false;isEditDisabled=true;isEditActive= false;selectedIds=[] ;if(selectedIds){isDisabled=true} ;
-    selectedIds = []}" >Delete</button>
+    <div class="flex justify-between w-full">
+      <div class="flex">
+        <button :disabled="isDisabled"
+                class="rounded-md px-3 py-1 transition duration-300  mb-4 subpixel-antialiased	font-medium	"
+                :class="{'bg-red-600 text-white hover:bg-red-700':isActive,'bg-stone-200 text-stone-600':!isActive}"
+                @click="()=>{$emit(deleteMultiple[0]['onClickEvent'],selectedIds);isDisabled = true;isActive=false;isEditDisabled=true;isEditActive= false;selectedIds=[] ;if(selectedIds){isDisabled=true} ;
+    selectedIds = []}">Delete
+        </button>
 
-            <button :disabled="isEditDisabled" class="ml-5 rounded-md px-3 py-1 transition duration-300  mb-4 subpixel-antialiased	font-medium" :class="{'bg-blue-600 text-white hover:bg-blue-700':isEditActive,'bg-stone-200 text-stone-600':!isEditActive}" @click="()=>{$emit(edit[0]['onClickEvent'],selectedIds);isDisabled=true;isEditActive= false;isActive=false;isDisabled=true;selectedIds = [];if(selectedIds){isEditDisabled=true} }">Edit</button>
-
-        </div>
-        <div @click="isHidden = !isHidden">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-  </svg>
+        <button :disabled="isEditDisabled"
+                class="ml-5 rounded-md px-3 py-1 transition duration-300  mb-4 subpixel-antialiased	font-medium"
+                :class="{'bg-blue-600 text-white hover:bg-blue-700':isEditActive,'bg-stone-200 text-stone-600':!isEditActive}"
+                @click="()=>{$emit(edit[0]['onClickEvent'],selectedIds);isDisabled=true;isEditActive= false;isActive=false;isDisabled=true;selectedIds = [];if(selectedIds){isEditDisabled=true} }">
+          Edit
+        </button>
 
       </div>
-        
+      <div @click="isHidden = !isHidden">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+             class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
+        </svg>
       </div>
-      
-      <div class="block w-full mb-2 bg-stone-100 w-1/4 mt-2" :class="{'hidden':isHidden}" >
+    </div>
+
+    <div class="block w-full mb-2 bg-stone-100 w-1/4 mt-2" :class="{'hidden':isHidden}">
           <span v-for="(item,i) in search" :key="i" class="flex ml-2">          
-            <input :type="item.type" class="w-full border-2 border-stone-400 bg-stone-200  h-8 m-3" 
-            v-model="searchInput[item.paramNumber]" :placeholder="item['searchParameter']" @input="$emit(item['searchParamType'],searchInput)">
+            <input :type="item.type" class="w-full border-2 border-stone-400 bg-stone-200  h-8 m-3"
+                   v-model="searchInput[item.paramNumber]" :placeholder="item['searchParameter']"
+                   @input="$emit(item['searchParamType'],searchInput)">
           </span>
-        </div>
-    
+    </div>
+
     <table class="table-auto border-collapse border w-full">
-      
+
       <thead>
       <tr class="border-0 border-y-2 border-t-0 border-slate-500 bg-neutral-300">
         <th class="text-left px-3 pt-4 pb-2 font-bold"
@@ -37,13 +46,14 @@
         </th>
       </tr>
       </thead>
-      
+
       <tbody class="font-semibold">
       <tr v-if="tableRows.length === 0">
         <td colspan="100%" class="text-center pt-2 text-slate-700">No Data To Display.</td>
       </tr>
       <tr v-for="row in tableRows" :key="row[0]" class="border-y border-slate-400 bg-neutral-100 hover:bg-neutral-200">
-        <input type="checkbox" class="ml-3" :value="row[0]" v-model="selectedIds" v-on:change="()=>{isChecked();isMultipleChecked()}">
+        <input type="checkbox" class="ml-3" :value="row[0]" v-model="selectedIds"
+               v-on:change="()=>{isChecked();isMultipleChecked()}">
         <td v-for="data in row" class="px-3 py-1 text-slate-800">
           <span v-if="data === null || data === ''">None</span>
           <span v-else>{{ data }}</span>
@@ -64,7 +74,7 @@
 </template>
 
 <script setup>
-import {defineProps,ref} from "vue";
+import {defineProps, ref} from "vue";
 
 let selectedIds = ref([])
 let isDisabled = ref(true)
@@ -74,29 +84,36 @@ let isEditActive = ref(false)
 let searchInput = ref({})
 let isHidden = ref(true)
 
-function isMultipleChecked(){
+function isMultipleChecked() {
 
-  if(selectedIds.value.length === 1){
+  if (selectedIds.value.length === 1) {
     isEditDisabled.value = false
     isEditActive.value = true
-  }else{
+  } else {
     isEditDisabled.value = true
     isEditActive.value = false
   }
 }
 
-function isChecked(){
-    
-  if(selectedIds.value.length === 0){
+function isChecked() {
+
+  if (selectedIds.value.length === 0) {
     isActive.value = false
     isDisabled.value = true
-  }else{
+  } else {
     isActive.value = true
     isDisabled.value = false
   }
 }
 
-let {tableColumns, tableRows, actions, deleteMultiple,edit,search} = defineProps(['tableColumns', 'tableRows', 'actions','deleteMultiple','edit','search'])
+let {
+  tableColumns,
+  tableRows,
+  actions,
+  deleteMultiple,
+  edit,
+  search
+} = defineProps(['tableColumns', 'tableRows', 'actions', 'deleteMultiple', 'edit', 'search'])
 
 let modificationsColum = tableColumns.pop()
 </script>
