cd3f217d47bb5a118080b506587f34ab60b7d643
gehirudm <gehirudm@pm.me>
2023-08-25 19:09:20 +0530

    Working on the LedgerRecord component

--- accounting-web/src/composibles/entity/ledger-records.js
+++ accounting-web/src/composibles/entity/ledger-records.js
@@ -11,7 +11,7 @@ export function useLedgerRecords() {
 
     /** Notification provider has to be inject here, which will most likely be run at setup() function of a Component. (inject() can only be used in setup()) */
     const notificationInjection = useNotifications().injectNotifications()
-    
+
     /**
      * Converts the Raw response from the server into a entity object
      * @param {import("../../types").RawLedgerRecord} rawLedgerRecord Raw response from the backend.
@@ -53,6 +53,45 @@ export function useLedgerRecords() {
             whenever(logicAnd(isFinished, success), () => {
                 const rawLedgerRecords = /** @type {import("../../types").RawLedgerRecord[] }*/(toValue(data).records);
                 resolve(rawLedgerRecords.map(serialize));
+            })
+            whenever(logicAnd(isFinished, logicNot(success)), () => reject())
+        })
+    }
+
+    /**
+     * Get ledger records filtred by date or narration
+     * @param {number} limit
+     * @param {number} page
+     * @param {Date} [data]
+     * @param {string} [narration]
+     * @returns {Promise<import("../../types").LedgerRecord[]>}
+     */
+    const getLedgerRecordsFiltered = async (limit, page, data, narration) => {
+        return new Promise((resolve, reject) => {
+            const queryString = [`limit=${limit}`, `page`]
+            const success = ref(false);
+            const { data, isFinished } = useAuthorizedFetch(`/general-ledger`, 'Get Ledger Records', success, notificationInjection).json().get();
+
+            whenever(logicAnd(isFinished, success), () => {
+                const rawLedgerRecords = /** @type {import("../../types").RawLedgerRecord[] }*/(toValue(data).records);
+                resolve(rawLedgerRecords.map(serialize));
+            })
+            whenever(logicAnd(isFinished, logicNot(success)), () => reject())
+        })
+    }
+
+    /**
+     * Get ledger record count
+     * @returns {Promise<number>}
+     */
+    const getLedgerRecordCount = async () => {
+        return new Promise((resolve, reject) => {
+            const success = ref(false);
+            const { data, isFinished } = useAuthorizedFetch(`/general-ledger`, 'Get Ledger Records', success, notificationInjection).json().get();
+
+            whenever(logicAnd(isFinished, success), () => {
+                const recordCount = /** @type {number}*/(toValue(data).record_count);
+                resolve(recordCount);
             })
             whenever(logicAnd(isFinished, logicNot(success)), () => reject())
         })
@@ -109,5 +148,5 @@ export function useLedgerRecords() {
         })
     }
 
-    return { addLedgerRecord, getLedgerRecords, getLedgerRecordsByDate, getLedgerRecordsByNarration }
+    return { addLedgerRecord, getLedgerRecords, getLedgerRecordCount, getLedgerRecordsByDate, getLedgerRecordsByNarration }
 }
