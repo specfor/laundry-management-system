e2a731f092822b19afd0362bbb2a25e85ca0d34f
vihanga <vihanga2003nimsara@gmail.com>
2023-08-25 19:17:57 +0530

    user_status table migration

---
+++ api/migrations/m00016_userStatus_update.php
@@ -0,0 +1,34 @@
+<?php
+
+use LogicLeap\PhpServerCore\MigrationScheme;
+
+class m00016_userStatus_update extends MigrationScheme
+{
+    public static function isReversible(): bool
+    {
+        return false;
+    }
+
+    public static function up(): bool
+    {
+        $sql = "alter table user_status drop primary key;
+                alter table user_status add column id int auto_increment primary key;";
+        try {
+            self::$pdo->exec($sql);
+            return true;
+        } catch (Exception) {
+            return false;
+        }
+    }
+
+    public static function down(): bool
+    {
+        $sql = "";
+        try {
+            self::$pdo->exec($sql);
+            return true;
+        } catch (Exception) {
+            return false;
+        }
+    }
+}
