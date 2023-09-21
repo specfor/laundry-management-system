e887cc677fd3cdc0d71b68ac7d63d362ad213043
gehirudm <gehirudm@pm.me>
2023-09-21 13:08:27 +0530

    add vue-virtual-scroller package

--- cashier-web/components.d.ts
+++ cashier-web/components.d.ts
@@ -7,12 +7,20 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    copy: typeof import('./src/components/NotificationPopover copy.vue')['default']
+    IconMdiArchiveRefreshOutline: typeof import('~icons/mdi/archive-refresh-outline')['default']
     IconMdiBell: typeof import('~icons/mdi/bell')['default']
-    IconMdiBellOutline: typeof import('~icons/mdi/bell-outline')['default']
+    IconMdiClockTimeFourOutline: typeof import('~icons/mdi/clock-time-four-outline')['default']
     IconMdiCog: typeof import('~icons/mdi/cog')['default']
     IconMdiConnection: typeof import('~icons/mdi/connection')['default']
+    IconMdiDelete: typeof import('~icons/mdi/delete')['default']
+    IconMdiPin: typeof import('~icons/mdi/pin')['default']
+    IconMdiPinOutline: typeof import('~icons/mdi/pin-outline')['default']
+    ItemPanel: typeof import('./src/components/ItemPanel.vue')['default']
+    ItemSearcher: typeof import('./src/components/ItemSearcher.vue')['default']
+    ItemTable: typeof import('./src/components/ItemTable.vue')['default']
+    ModelWithInput: typeof import('./src/components/ModelWithInput.vue')['default']
     NotificationPopover: typeof import('./src/components/NotificationPopover.vue')['default']
+    PaymentModel: typeof import('./src/components/PaymentModel.vue')['default']
     Popover: typeof import('./src/components/Popover.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
